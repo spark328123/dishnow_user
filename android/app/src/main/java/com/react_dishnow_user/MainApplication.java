b3e54c593a5ed32a3dd41a049ce9a7f8f439341c
성빈 <spark328123@gmail.com>
2019-07-03 17:21:25 +0900

    ios_kakao_login

--- android/app/src/main/java/com/react_dishnow_user/MainApplication.java
+++ android/app/src/main/java/com/react_dishnow_user/MainApplication.java
@@ -3,6 +3,7 @@ package com.react_dishnow_user;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dooboolab.kakaologins.RNKakaoLoginsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNKakaoLoginsPackage()
       );
     }
 
