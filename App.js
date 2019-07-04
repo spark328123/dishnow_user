76585143c536f3f5803df7b03cfabc09cc2be7d4
jdb0ss <smhong1995@naver.com>
2019-07-04 22:19:24 +0900

    123

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ import {
   Image
 } from "react-native";
 import { thisTypeAnnotation } from "@babel/types";
+import Loginkakao from "./login_kakao";
 
 const { height, width } = Dimensions.get("window");
 export default class App extends Component {
@@ -28,43 +29,6 @@ export default class App extends Component {
     this.state = {
       isKakaoLogging: false
     };
-    if (!RNKakaoLogins) {
-      console.log("Not Linked");
-    }
-  }
-
-  // 카카오 로그인 시작.
-  kakaoLogin() {
-    console.log("   kakaoLogin   ");
-    RNKakaoLogins.login((err, result) => {
-      if (err) {
-        return;
-      }
-      console.log("Login!", result);
-    });
-  }
-
-  kakaoLogout() {
-    console.log("   kakaoLogout   ");
-    RNKakaoLogins.logout((err, result) => {
-      if (err) {
-        console.log("error", err.toString());
-        return;
-      }
-      console.log("Logout!", result);
-    });
-  }
-
-  // 로그인 후 내 프로필 가져오기.
-  getProfile() {
-    console.log("getKakaoProfile");
-    RNKakaoLogins.getProfile((err, result) => {
-      if (err) {
-        console.log("error", err.toString());
-        return;
-      }
-      console.log("result", result);
-    });
   }
 
   render() {
@@ -99,20 +63,7 @@ export default class App extends Component {
           <View style={styles.snsLoginContainer}>
             <Text style={styles.snsText}>SNS 로그인</Text>
             <View style={styles.snsButtonContainer}>
-              <TouchableOpacity
-                onPress={() => {
-                  this.kakaoLogin();
-                }}
-              >
-                <Image
-                  isLoading={this.state.isNaverLoggingin}
-                  onPress={() => this.kakaoLogin()}
-                  activeOpacity={0.5}
-                  style={styles.btnKakaoLogin}
-                  textStyle={styles.txtNaverLogin}
-                  source={require("./assets/kakaolink_btn_medium.png")}
-                />
-              </TouchableOpacity>
+              <Loginkakao />
               <TouchableOpacity>
                 <Image
                   onPress={() => {
